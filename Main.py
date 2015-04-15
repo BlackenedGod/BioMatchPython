a77605e22300ec08ff6d1b460aae71c882776da2
altugyigit <altug_yigit@hotmail.com>
2015-04-15 23:15:21 +0300

    Main.py eklendi instance sorunu gideriliyor.Rev 2

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ jsoninstance.downloadLastImageCanak()
 
 tacImagePath = "TestImg/picTac.jpg"
 canakImagePath = "TestImg/picCanak.jpg"
-maskFilePath = "MaskImg/"+"mask_" + jsoninstance.getNowTime()
+maskFilePath = "MaskImg/"+"mask_" , jsoninstance.getNowTime()
 
 #******************Tac ve Canak icin maske cikar MaskImg ye kayit et*******************
 maskingTacinstance = MaskingClass.maskingClass(maskFilePath, tacImagePath)
