d9e84c0be873672941a007e7c0a6b31f76b16a77
Altuğ Yiğit <altug_yigit@hotmail.com>
2015-05-21 20:05:32 +0300

    Update RabbitMQReceive.py
    id Chanel olarak değiştirildi.

--- RabbitMQReceive.py
+++ RabbitMQReceive.py
@@ -8,13 +8,13 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('188.166.67.19'))
 
 channel = connection.channel()
 
-channel.queue_declare(queue='url')
+channel.queue_declare(queue='id')
 
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
 
 channel.basic_consume(callback,
-                      queue='url',
+                      queue='id',
                       no_ack=True)
 
 channel.start_consuming()
