b89c3b6d7c7f05e5acec4bd0c329bd471e9d563b
Altuğ Yiğit <altug_yigit@hotmail.com>
2015-05-21 21:53:00 +0300

    Update RabbitMQSend.py
    Id değiştirildi

--- RabbitMQSend.py
+++ RabbitMQSend.py
@@ -8,10 +8,10 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('188.166.67.19'))
 
 channel = connection.channel()
 
-channel.queue_declare(queue='id')
+channel.queue_declare(queue='obid')
 
 channel.basic_publish(exchange='',#Kuyruk ismine gore convert edicek mesaji yollayacak bos default.
-                      routing_key='id',
+                      routing_key='obid',
                       body='FOTO URL !')
 
 print " [x] Sent 'Burada URL olacak !'"
