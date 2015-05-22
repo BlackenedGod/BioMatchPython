04c0ae3aa55dec6761d18a11171f702986a8024d
Altuğ Yiğit <altug_yigit@hotmail.com>
2015-05-22 11:42:40 +0300

    Update RabbitMQReceive.py
    idob

--- RabbitMQReceive.py
+++ RabbitMQReceive.py
@@ -8,13 +8,13 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('188.166.67.19'))
 
 channel = connection.channel()
 
-channel.queue_declare(queue='obid')
+channel.queue_declare(queue='idob')
 
 def callback(ch, method, properties, body):
     print " [x] Received %r" % (body,)
 
 channel.basic_consume(callback,
-                      queue='obid',
+                      queue='idob',
                       no_ack=True)
 
 channel.start_consuming()
