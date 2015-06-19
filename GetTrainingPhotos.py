549ef0401c4c3aff099bde025a8ee422e2b86ef2
Kerem Kambur <keremkambur@yandex.com>
2015-06-19 03:33:14 +0300

    rev2

---
+++ GetTrainingPhotos.py
@@ -0,0 +1,61 @@
+__author__ = 'kerem'
+
+import json, httplib, urllib
+import datetime
+import time
+import Image
+import cv2
+from CalcDistance import CalcDistance
+from MaskingClass import maskingClass
+
+class GetTrainingPhotos:
+
+    def __init__(self):
+        #Bu Strinlerin tanimlanmasi ileride degisebilecekleri anlamina gelir JSON objelerinin alanlarinin isimleridir.
+        self.tacYaprakString = "TacYaprak"
+        self.canakYaprakString = "CanakYaprak"
+        self.resultsString = "results"
+        self.createdAtString = "createdAt"
+        self.urlString = "url"
+        self.canakURLPath = "TestImg/picCanak.jpg"
+        self.focalLengthString = "focalLength"
+        self.sensorSize = ""
+        self.objIDString = "objectId"
+        self.sensorSizeString = "sensorSize"
+        self.tacURLPath = "TestImg/picTac.jpg"
+        self.specyName = "specy"
+        self.locationParam = urllib.urlencode({"where": json.dumps({
+            "location": {
+                "$exists" : True
+            }
+        })})
+        try:
+            self.connection = httplib.HTTPSConnection("api.parse.com", 443)
+            self.connection.connect()
+
+            self.connection.request('GET', '/1/classes/Train?%s' % self.locationParam, json.dumps({
+            }), {
+                "X-Parse-Application-Id": "HgrrtDO2dnazkQCPY59MR82ERhiamS5b1LTXBit8",
+                "X-Parse-REST-API-Key": "hKSwFIi8Y6CrijPpXUABQSrLj5TMVekMwqk2I98I",
+            })
+
+            self.result = json.loads(self.connection.getresponse().read())
+            self.dump = json.dumps(self.result) # Komple String sonucu icin .
+            self.JSONObjectresult = self.result[self.resultsString] # Tum JSON objeleri icin .
+            print self.dump
+        except RuntimeError:
+            print 'Baglanti Kurulamadi !'
+
+
+    def downloadAll(self):
+
+        length = len(self.JSONObjectresult)
+        for i in range(0, length):
+            urlInfoTac = self.result[self.resultsString][i][self.objIDString][self.canakYaprakString][self.urlString]
+            urlInfoCanak = self.result[self.resultsString][i][self.objIDString][self.canakYaprakString][self.urlString]
+            specyInfo = self.result[self.resultsString][i][self.objIDString]
+            dateTimePath = datetime.datetime.strptime(self.result[self.resultsString][i][self.createdAtString], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S.%f")
+            urllib.urlretrieve(urlInfoTac, self.tacURLPath+dateTimePath+".png")
+            urllib.urlretrieve(urlInfoCanak, self.canakURLPath+dateTimePath+".png")
+            maskFilePathTac = "MaskImg/"+"mask_tac_"+str(i)+".png"
+            maskFilePathCanak = "MaskImg/"+"mask_canak_"+str(i)+".png"
